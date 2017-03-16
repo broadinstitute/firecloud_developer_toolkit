0b07b05ebf6e272bf0cb92854e025693734de42d
Gordon Saksena <gsaksena@broadinstitute.org>
2017-03-16 11:56:04 -0400

    Add option to shut down current node

--- gce/stop_vm.sh
+++ gce/stop_vm.sh
@@ -2,7 +2,12 @@
 
 #stop vm, can be restarted later
 #charges stop accruing for CPU time, but still accrue for the disk space
+# omit the instance name to shut down the node you are currently on.
 
-instance_name=$1
+instance_name="$1"
 
-gcloud compute instances stop $instance_name
+if [ -z "$instance_name"]; then 
+    sudo shutdown -h now
+else
+    gcloud compute instances stop $instance_name
+fi
