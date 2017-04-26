9d353ca66c2e6f7f1d8d9cf6b218b4670a830953
Gordon Saksena <gsaksena@broadinstitute.org>
2017-04-26 13:38:01 -0400

    changed executable bit, added cromwell 26

--- install/install_cromwell_26_nr.sh
+++ install/install_cromwell_26_nr.sh
@@ -9,8 +9,8 @@ fi
 
 mkdir -p $FDT_BIN_ROOT
 cd /tmp
-curl -L https://github.com/broadinstitute/cromwell/releases/download/25/cromwell-25.jar > cromwell.tmp.jar
+curl -L https://github.com/broadinstitute/cromwell/releases/download/26/cromwell-26.jar > cromwell.tmp.jar
 mv cromwell.tmp.jar $FDT_BIN_ROOT/cromwell.jar
-curl -L https://github.com/broadinstitute/wdltool/releases/download/0.8/wdltool-0.8.jar > wdltool.tmp.jar
+curl -L https://github.com/broadinstitute/wdltool/releases/download/0.10/wdltool-0.10.jar > wdltool.tmp.jar
 mv wdltool.tmp.jar $FDT_BIN_ROOT/wdltool.jar
 
