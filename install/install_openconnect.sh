3cc6a9be38cf7db290b1fafa28d418ec7dcf4f92
Gordon Saksena <gsaksena@broadinstitute.org>
2017-05-16 11:44:42 -0400

    add install_openconnect

---
+++ install/install_openconnect.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install openconnect
+
+# to connect, run somethnig like:
+# sudo openconnect  --authgroup=Duo-Split-Tunnel-VPN vpn.broadinstitute.org
