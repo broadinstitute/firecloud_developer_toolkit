3d681d1f97733cf4d65ff3728ee3fe4716c07c63
gsaksena <gsaksena@broadinstitute.org>
2017-11-29 21:16:28 -0500

    fixes for java 8 installer

--- install/install_oraclejavajdk_8_ubuntu.sh
+++ install/install_oraclejavajdk_8_ubuntu.sh
@@ -2,10 +2,14 @@
 
 #install java 8 on ubuntu
 
+# install package containing add-apt-repository
+sudo apt-get install -y software-properties-common
+
+
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
+sudo apt-get install -y oracle-java8-installer
 
 # set this version of Java as the default
-sudo apt-get install oracle-java8-set-default
+sudo apt-get install -y oracle-java8-set-default
